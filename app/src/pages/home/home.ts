4fad461aaa5529cd4917dd38bb3615e3dfb13d8f
haganicolau <haganicolau@gmail.com>
2018-10-24 09:53:27 -0300

    subindo alterações lista de usuarios sem conexao com api

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ListaUsuarioPage } from '../lista-usuario/lista-usuario';
 
 @Component({
   selector: 'page-home',
@@ -7,6 +8,7 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  listaUsuarios = ListaUsuarioPage;
   constructor(public navCtrl: NavController) {
 
   }
---
+++ app/src/pages/lista-usuario/lista-usuario.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Usuario } from '../../modules/usuario';
+
+/**
+ * Generated class for the ListaUsuarioPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-lista-usuario',
+  templateUrl: 'lista-usuario.html',
+})
+export class ListaUsuarioPage {
+
+  usuarios: Usuario[];
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams
+  ) {
+
+    this.usuarios = [
+      {id: 1, name: "Nome 1", email: "email1@email.com", profile: 1},
+      {id: 2, name: "Nome 2", email: "email2@email.com", profile: 1},
+      {id: 3, name: "Nome 3", email: "email3@email.com", profile: 1}
+    ];
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ListaUsuarioPage');
+  }
+
+}
