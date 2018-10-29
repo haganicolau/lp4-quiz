9b20ce90a28cf547ee0413f1f4d984988faa623d
haganicolau <haganicolau@gmail.com>
2018-10-29 00:10:23 -0300

    cadastro de usuario

--- app/src/pages/cadastro-usuario/cadastro-usuario.ts
+++ app/src/pages/cadastro-usuario/cadastro-usuario.ts
@@ -1,13 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Usuario } from '../../modules/usuario';
-
-/**
- * Generated class for the CadastroUsuarioPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { HttpClient } from '@angular/common/http';
 
 @IonicPage()
 @Component({
@@ -16,14 +10,72 @@ import { Usuario } from '../../modules/usuario';
 })
 export class CadastroUsuarioPage {
 
-  private usuario: Usuario;
+  private orderForm;
+  public usuario: Usuario;
+  private senha: string;
+  private senha_confirma:string;
+  private error = { condicao: false, message:''};
+  private success = { condicao: false, message: ''};
 
   constructor(
     public navCtrl: NavController, 
-    public navParams: NavParams
+    public navParams: NavParams,
+    public http: HttpClient,
   ) {
     this.usuario = new Usuario();
+  }
 
+  salvar(){
+    this.error.condicao = false;
+    this.validarDados();
+    console.log(this.error.condicao);
+    
+    if(!this.error.condicao){
+      this.usuario.senha = this.senha;
+
+      this.http.post("http://localhost:3000/usuario", 
+      this.usuario
+        ).subscribe(res => {
+          console.log(res);
+          this.error.condicao = false;
+          this.error.message = '';
+          this.success.condicao = true;
+          this.success.message = "Criado com sucesso"
+
+          this.navCtrl.push(CadastroUsuarioPage.name)
+          
+        }, (err) => {
+          console.log(err);
+        });
+
+    }
+  }
+
+  validarDados(){
+    if(!this.usuario.nome){
+      this.error.condicao = true;
+      this.error.message = "Nome, campo obrigatório!";
+    }
+
+    if(!this.usuario.email){
+      this.error.condicao = true;
+      this.error.message = "Email, campo obrigatório!";
+    }
+
+    if(!this.senha || !this.senha_confirma){
+      this.error.condicao = true;
+      this.error.message = "Senha e confirma senha, campos obrigatórios!";
+    }
+
+    if(this.senha !== this.senha_confirma){
+      this.error.condicao = true;
+      this.error.message = "Senha e confirma senha devem ser iguais";
+    }
+
+    if(!this.usuario.perfil){
+      this.error.condicao = true;
+      this.error.message = "Perfil, campo obrigatório!";
+    }
   }
 
   ionViewDidLoad() {
